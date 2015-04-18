f2b745e0c039637ca74b2c3d429f31374d7fa7d2
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-17 23:29:30 -0700

    Milestone 10
    I fixed the error!!!!!!!!!!!!!!!!!1

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 
 			if (new_task != '') {
 				$.post('includes/add-task.php', {task: new_task}, function(data){
-					$(('add-new-task input[name=new-task]').val();
+					$('add-new-task input[name=new-task]').val();
 						$(data).appendTo('task-list ul'). hide().fadeIn();
 				});
 			}
