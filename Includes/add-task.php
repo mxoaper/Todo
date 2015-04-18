eab7f13489914f493c852ab4b2cf4a3bc387f60d
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-17 23:41:55 -0700

    Milestone 11
    I created add-task.php in the includes folder

---
+++ Includes/add-task.php
@@ -0,0 +1,15 @@
+<?php 
+	$task = strip_tags($_POST['task']};
+	$date = date('Y-m-d');
+	$time = date('H:1:s');
+
+	include('connect.php');
+
+	$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+	$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time")");
+
+	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time' ";
+
+
+
+ ?>
